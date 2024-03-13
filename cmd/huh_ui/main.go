2223b231191db979e8f352c40fbc9af02d9e7c33
Johan van Rhyn <johan@squarehole.dev>
2024-03-13 21:42:00 +0200

    Fixed linting issues

--- cmd/huh_ui/main.go
+++ cmd/huh_ui/main.go
@@ -1,1 +1,95 @@
-package huh_ui
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/charmbracelet/huh/spinner"
+
+	"github.com/charmbracelet/huh"
+	"github.com/charmbracelet/log"
+)
+
+type (
+	GeoInfo struct {
+		IPAddress string
+		City      string `json:"city"`
+		Region    string `json:"region"`
+		Country   string `json:"country"`
+	}
+)
+
+var ipAddress string
+
+func main() {
+	form := huh.NewForm(
+		huh.NewGroup(
+			huh.NewInput().
+				Title("IP Address").
+				Value(&ipAddress)))
+
+	err := form.Run()
+	if err != nil {
+		log.Error("Error", err)
+	}
+
+	var geoInfo *GeoInfo
+
+	action := func() {
+		geoInfo, err = getGeoInfo(ipAddress)
+		if err != nil {
+			log.Error(err)
+		}
+	}
+
+	_ = spinner.New().
+		Title("Looking up IP-Address").
+		Action(action).
+		Run()
+
+	form = huh.NewForm(
+		huh.NewGroup(
+			huh.NewText().Title("IP Address").Lines(1).
+				Value(&ipAddress),
+			huh.NewText().Title("Country").Lines(1).
+				Value(&geoInfo.Country),
+			huh.NewText().Title("Region").Lines(1).
+				Value(&geoInfo.Region),
+			huh.NewText().Title("City").Lines(1).
+				Value(&geoInfo.City)))
+
+	err = form.Run()
+	if err != nil {
+		log.Error(err)
+	}
+}
+
+func getGeoInfo(ipAddress string) (*GeoInfo, error) {
+	time.Sleep(time.Second * 2)
+	resp, err := http.Get(fmt.Sprintf("http://localhost:3000/api/lookup/%s", ipAddress))
+	if err != nil {
+		return nil, err
+	}
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Error(err)
+		}
+	}(resp.Body)
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var geoInfo GeoInfo
+	err = json.Unmarshal(body, &geoInfo)
+	if err != nil {
+		return nil, err
+	}
+
+	return &geoInfo, nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/jvanrhyn/brgeo
 go 1.21.1
 
 require (
+	github.com/charmbracelet/bubbles v0.18.0 // indirect
+	github.com/charmbracelet/huh v0.3.0
 	github.com/charmbracelet/log v0.3.1
 	github.com/go-errors/errors v1.5.1
 	github.com/gofiber/fiber/v2 v2.52.0
@@ -16,8 +18,13 @@ require (
 
 require (
 	github.com/andybalholm/brotli v1.1.0 // indirect
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/catppuccin/go v0.2.0 // indirect
+	github.com/charmbracelet/bubbletea v0.25.0 // indirect
+	github.com/charmbracelet/huh/spinner v0.0.0-20240306161957-71f31c155b08
 	github.com/charmbracelet/lipgloss v0.9.1 // indirect
+	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -30,7 +37,10 @@ require (
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-localereader v0.0.1 // indirect
 	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/ansi v0.0.0-20230316100256-276c6243b2f6 // indirect
+	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.15.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
@@ -43,5 +53,6 @@ require (
 	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/term v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
