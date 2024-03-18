ce17a58b115ee71d0e57fe42abcc33c333afcb51
Johan van Rhyn <johan@squarehole.dev>
2024-03-18 21:48:26 +0200

    Refactor Model struct methods
    Consolidate Model struct methods and add IP validation function. New and Init methods initialize the model and start the spinner ticking. Update method handles messages and view method renders the model. Added validateIP function for IP address validation. The changes aim to enhance functionality and maintain code coherence.

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -63,6 +63,9 @@ func main() {
 	}
 }
 
+// New creates and returns a new Model instance with default values.
+// It initializes a text input with a placeholder for entering an IP address,
+// a spinner for indicating loading status, and sets loading to false.
 func New() Model {
 	ti := textinput.New()
 	ti.Placeholder = "Enter IP Address"
@@ -81,10 +84,18 @@ func New() Model {
 	}
 }
 
+// Init is a method of the Model struct that initializes the model.
+// It returns a command that starts the spinner ticking.
 func (m Model) Init() tea.Cmd {
 	return m.spinner.Tick
 }
 
+// Update is a method of the Model struct that handles incoming messages and updates the model accordingly.
+// It returns the updated model and a command.
+// It handles key presses, geolocation responses, and spinner ticks.
+// When the Enter key is pressed, it validates the input IP address and initiates a geolocation lookup if the IP is valid.
+// When a geolocation response is received, it updates the geolocation info in the model and clears any errors.
+// When a spinner tick message is received, it updates the spinner in the model.
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
@@ -130,6 +141,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
+// View is a method of the Model struct that returns a string representation of the model.
+// It defines styles for different parts of the view and uses these styles to render the view.
+// The view includes a title, an input field (when not loading), an error message (if any), a loading spinner (when loading), and the geolocation data (when available).
+// The method uses fmt.Sprintf for string formatting.
 func (m Model) View() string {
 	// Define styles
 	titleStyle := lipgloss.NewStyle().Bold(true).Foreground(lipgloss.Color("#008ACC"))
@@ -142,33 +157,34 @@ func (m Model) View() string {
 
 	if !m.loading {
 		// Build the view string with styles
-		inputView += "\n\n" + m.textinput.View() + "\n\n"
+		inputView += fmt.Sprintf("\n\n%s\n\n", m.textinput.View())
 	}
 
 	if m.err != nil {
-		errorView := staticTextStyle.Render("Error : ") + errorStyle.Render(m.err.Error())
-		return titleView + errorView + inputView
+		errorView := fmt.Sprintf("%s %s", staticTextStyle.Render("Error : "), errorStyle.Render(m.err.Error()))
+		return fmt.Sprintf("%s%s%s", titleView, errorView, inputView)
 	}
 
 	spinnerView := ""
 	if m.loading {
 		spinnerView += fmt.Sprintf("\n\n%s Looking up Geo location.\n\n", m.spinner.View())
-
 	}
 
 	dataView := ""
 
 	if m.geoInfo.Country != "" {
-
-		dataView += "\n\n"
-		dataView += staticTextStyle.Render("Looking up: ") + valueStyle.Render(m.geoInfo.IPAddress) + "\n" +
-			staticTextStyle.Render("Region: ") + valueStyle.Render(m.geoInfo.Region) + "\n" +
-			staticTextStyle.Render("City: ") + valueStyle.Render(m.geoInfo.City) + "\n" +
-			staticTextStyle.Render("Country: ") + valueStyle.Render(m.geoInfo.Country)
+		dataView += fmt.Sprintf("\n\n%s %s\n%s %s\n%s %s\n%s %s",
+			staticTextStyle.Render("Looking up: "), valueStyle.Render(m.geoInfo.IPAddress),
+			staticTextStyle.Render("Region: "), valueStyle.Render(m.geoInfo.Region),
+			staticTextStyle.Render("City: "), valueStyle.Render(m.geoInfo.City),
+			staticTextStyle.Render("Country: "), valueStyle.Render(m.geoInfo.Country))
 	}
-	return titleView + spinnerView + dataView + inputView
+	return fmt.Sprintf("%s%s%s%s", titleView, spinnerView, dataView, inputView)
 }
 
+// handleGeoLookup is a function that takes an IP address as a string and returns a command that fetches the geolocation information for that IP.
+// It calls the getGeoInfo function to fetch the geolocation information and logs any errors that occur during this process.
+// It then returns a GeoResponseMsg with the fetched geolocation information and any error that occurred.
 func handleGeoLookup(v string) tea.Cmd {
 	return func() tea.Msg {
 		geo, err := getGeoInfo(v)
@@ -183,6 +199,11 @@ func handleGeoLookup(v string) tea.Cmd {
 	}
 }
 
+// getGeoInfo is a function that takes an IP address as a string and returns the geolocation information for that IP and any error that occurred.
+// It makes a GET request to a local API endpoint with the IP address and reads the response.
+// If the response status is OK, it reads the response body and unmarshals it into a GeoInfo struct.
+// If any error occurs during this process, it retries the request up to a maximum number of times with an exponential backoff delay.
+// If it still fails after the maximum number of retries, it returns an empty GeoInfo struct and an error indicating the failure.
 func getGeoInfo(ipAddress string) (GeoInfo, error) {
 	var geoInfo GeoInfo
 	var err error
@@ -212,6 +233,10 @@ func getGeoInfo(ipAddress string) (GeoInfo, error) {
 	return GeoInfo{}, fmt.Errorf("failed to get geo info after %d attempts: %v", maxRetries, err)
 }
 
+// validateIP is a function that takes an IP address as a string and returns a boolean indicating whether the IP address is valid.
+// It uses the net.ParseIP function to parse the IP address and checks if the result is nil (indicating an invalid IP address).
+// If the result is not nil, it checks if the IP address is IPv4 or IPv6 using the To4 and To16 methods respectively.
+// If the IP address is either IPv4 or IPv6, it returns true; otherwise, it returns false.
 func validateIP(ip string) bool {
 	address := net.ParseIP(ip)
 	if address == nil {
