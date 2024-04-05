4d24292379c4383c8a8f07a51bbf9af13d5fd1d1
Johan van Rhyn <johan@squarehole.dev>
2024-04-05 21:33:44 +0200

    Add detailed comments for main, init, and getEnvFilePath funcs
    The commit introduces a series of comments explaining in detail what the main, init, and getEnvFilePath functions in cmd/brgeo/main.go do. Each line or block of code is described with its role and how it fits into the overall function operation. This improves code readability and maintainability.

--- cmd/brgeo/main.go
+++ cmd/brgeo/main.go
@@ -15,6 +15,12 @@ import (
 	"github.com/jvanrhyn/brgeo/internal/api"
 )
 
+// init is the function that initializes the application by reading Configuration data
+// from the .env file in the project.
+// If the .env file cannot be found in the project directory, it attempts to find the file
+// using the getEnvFilePath function.
+// If the .env file still cannot be found, an error message is printed to the console and the application panics.
+// The function uses the godotenv package to load the .env file.
 func init() {
 
 	// Read Configuration data from the .env file in the project
@@ -28,6 +34,26 @@ func init() {
 	}
 }
 
+// main is the entry point of the application.
+// It initializes the logger, sets up the global logger with custom options,
+// and starts the application by calling the InitDatabase and StartAndServe functions.
+// It uses the slog package for logging.
+//
+// The logger is initialized with the log.New function and set with the provided options.
+// The SetLevel function sets the logging level to Debug.
+// The SetTimeFormat function sets the time format to time.Kitchen.
+// The SetReportTimestamp function enables reporting timestamps in logs.
+//
+// The slogger package is used to set the default logger to the created logger.
+//
+// The Info log message "Starting the application" is printed using the slog.Info function.
+//
+// The Debug log message "InitDatabase called" is printed using the slog.Debug function.
+// The InitDatabase function (api.InitDatabase) is then called to initialize the database.
+//
+// The StartAndServe function (controller.StartAndServe) is called to start and serve the application.
+//
+// This function does not return anything.
 func main() {
 	w := os.Stderr
 	handler := log.New(w)
@@ -46,6 +72,11 @@ func main() {
 	controller.StartAndServe()
 }
 
+// getEnvFilePath retrieves the absolute file path of the .env file in the directory
+// where the calling function is located.
+// If the caller information cannot be obtained, a message will be printed to the console.
+// The function combines the directory path and the name of the .env file using filepath.Join.
+// Returns the absolute file path of the .env file.
 func getEnvFilePath() string {
 	_, filename, _, ok := runtime.Caller(0)
 	if !ok {
